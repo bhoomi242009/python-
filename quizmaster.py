9d9bb88bdea3f6437ba6c0fced99ed466f2d0918
bhoomi242009 <bhoomi242009@gmail.com>
2026-08-15 13:52:45 +0300

    Add files via upload

--- quizmaster.py
+++ quizmaster.py
@@ -19,6 +19,7 @@ question_file_name = "questions.txt"
 mark_message = ""
 game_over = False
 answer_boxes = [answer_box1, answer_box2, answer_box3, answer_box4]
+questions = []
 question_count = 0
 question_index = 0
 
@@ -71,4 +72,56 @@ def read_questions_file():
         questions.append(question)
         question_count = question_count + 1
     q_file.close()
-    
+
+def read_next_question():
+    global question_index
+    question_index = question_index + 1
+    return questions.pop(0).split(",")
+
+def on_mouse_down(pos):
+    index = 1
+    for box in answer_boxes:
+        if box.collidepoint(pos):
+            if index == int(question[5]):
+                correct_answer()
+            else:
+                game_over()
+
+        if skip_box.collidepoint(pos):
+            skip_question()
+
+def correct_answer():
+    global score, question, time_left, questions 
+    if questions:
+        question = read_questions_file()
+        time_left = 10
+    else:
+        game_over()
+
+def game_over():
+    global is_game_over
+    message = f"Game over!\n You got {score} points."
+    question = [message, "-", "-", "-", "-", 5]
+    time_left = 0
+    is_game_over = True
+
+def skip_question():
+    global question, time_left
+    if question and not game_over:
+        question = read_next_question()
+        time_left = 10
+    else:
+        game_over()
+
+def update_time_left():
+    global time_left
+    if time_left:
+        time_left = time_left - 1
+    else:
+        game_over()
+
+read_questions_file()
+question = read_next_question()
+clock.schedule_interval(update_time_left, 1)
+
+pgzrun.go()
